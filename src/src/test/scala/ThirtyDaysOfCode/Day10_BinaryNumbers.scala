8c0394418f84c9c67c56bc5a848e195e99080458
anch <anton_chug@yahoo.com>
2016-07-13 19:15:37 +0300

    Added Days 10 and 11

---
+++ src/src/test/scala/ThirtyDaysOfCode/Day10_BinaryNumbers.scala
@@ -0,0 +1,72 @@
+package ThirtyDaysOfCode
+
+import Infrastructure.HackersHelper._
+import org.scalatest.FunSuite
+import scala.collection.mutable
+
+class Day10_BinaryNumbers extends FunSuite {
+
+  object Solution {
+
+    //import java.util.Scanner
+    //import scala.collection.mutable
+
+    def convertToBinary(n: Int) = {
+      val stack = mutable.Stack[Int]()
+      var N = n
+      while (N > 0) {
+        val remainder = N % 2
+        N = N / 2
+        stack.push(remainder)
+      }
+      stack.toList
+    }
+
+    def main(args: Array[String]) {
+
+      val sc = new Scanner(System.in)
+      val n = sc.nextInt()
+      if (n <= math.pow(10, 6) && n >= 1) {
+        val binaryNumbers = convertToBinary(n).mkString.split("0")
+        val sums = binaryNumbers.map(x => x.map(c => c.asDigit).sum)
+        Console.println(sums.max)
+      }
+    }
+  }
+
+  test("Run solution 1") {
+    val inputData = mutable.Queue(
+      "5"
+    )
+
+    val inputStream = new InputStream()
+    inputStream.setInputData(inputData)
+    System.setIn(inputStream)
+
+    val outputData = mutable.Queue(
+      "1"
+    )
+
+    Console.setOutputData(outputData)
+
+    Solution.main(null)
+  }
+
+  test("Run solution 2") {
+    val inputData = mutable.Queue(
+      "13"
+    )
+
+    val inputStream = new InputStream()
+    inputStream.setInputData(inputData)
+    System.setIn(inputStream)
+
+    val outputData = mutable.Queue(
+      "2"
+    )
+
+    Console.setOutputData(outputData)
+
+    Solution.main(null)
+  }
+}
---
+++ src/src/test/scala/ThirtyDaysOfCode/Day11_2D_Arrays.scala
@@ -0,0 +1,94 @@
+package ThirtyDaysOfCode
+
+import Infrastructure.HackersHelper._
+import org.scalatest.FunSuite
+
+import scala.collection.mutable
+
+class Day11_2D_Arrays extends FunSuite {
+
+  object Solution {
+
+    //import java.util.Scanner
+    import scala.collection.mutable.ArrayBuffer
+
+    class Matrix() {
+      val matrix: ArrayBuffer[ArrayBuffer[Int]] = new ArrayBuffer[ArrayBuffer[Int]]
+
+      def add(array: ArrayBuffer[Int]) = matrix += array
+
+      def hourGlassSum = matrix(0).sum + matrix(1)(1) + matrix(2).sum
+    }
+
+    def sliceJaggedArray(arr: Array[Array[Int]], slice: Int): ArrayBuffer[Matrix] = {
+
+      def subMatrix(arrays: Array[Array[Int]], rStart: Int, cStart: Int, rEnd: Int, cEnd: Int): Matrix = {
+        val matrix = new Matrix()
+        for (row <- rStart until rEnd) {
+          val hgRow = new ArrayBuffer[Int]()
+          for (col <- cStart until cEnd) {
+            hgRow += arrays(row)(col)
+          }
+          matrix.add(hgRow)
+        }
+        matrix
+      }
+
+      val result = new ArrayBuffer[Matrix]()
+
+      for (i <- arr.indices) {
+        for (j <- arr(i).indices) {
+          val rEnd = slice + i
+          val cEnd = slice + j
+          if (rEnd <= arr.length && cEnd <= arr.length) result += subMatrix(arr, i, j, rEnd, cEnd)
+        }
+      }
+
+      result
+    }
+
+    def main(args: Array[String]) {
+
+      val sc = new Scanner(System.in)
+      val array = Array.ofDim[Int](6, 6)
+
+      // Input
+      for (i <- 0 until 6) {
+        val numbers = sc.nextLine().split(" ").map(x => x.toInt)
+        for (k <- numbers.indices) {
+          if (numbers(k) <= 9 && numbers(k) >= -9) {
+            array(i)(k) = numbers(k)
+          }
+        }
+      }
+
+      // Calculations phase
+      val matrices = sliceJaggedArray(array, 3)
+      val max = matrices.map(h => h.hourGlassSum).max
+      Console.println(max)
+    }
+  }
+
+  test("Run solution") {
+    val inputData = mutable.Queue(
+      "1 1 1 0 0 0",
+      "0 1 0 0 0 0",
+      "1 1 1 0 0 0",
+      "0 0 2 4 4 0",
+      "0 0 0 2 0 0",
+      "0 0 1 2 4 0"
+    )
+
+    val inputStream = new InputStream()
+    inputStream.setInputData(inputData)
+    System.setIn(inputStream)
+
+    val outputData = mutable.Queue(
+      "19"
+    )
+
+    Console.setOutputData(outputData)
+
+    Solution.main(null)
+  }
+}
