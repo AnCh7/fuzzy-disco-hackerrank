9ba9e317b89f250fb61e2b5f2c91f9b0c554de74
anch <anton_chug@yahoo.com>
2016-07-20 22:58:41 +0300

    Added Days 17 and 18.

---
+++ src/src/test/scala/hackerrank/thirtydaysofcode/Day17_More_Exceptions.scala
@@ -0,0 +1,59 @@
+package hackerrank.thirtydaysofcode.scala
+
+import hackerrank.helper.scala._
+import org.scalatest.FunSuite
+
+import scala.collection.mutable
+
+class Day17_More_Exceptions extends FunSuite {
+
+  object Solution {
+
+    class Calculator() {
+      def power(n: Int, p: Int): Int = {
+        if (n < 0 || p < 0) throw new Exception("n and p should be non-negative")
+        else math.pow(n, p).toInt
+      }
+    }
+
+    def main(args: Array[String]) {
+      val myCalculator = new Calculator()
+      var T = StdIn.readLine().toInt
+
+      while (T > 0) {
+        val Array(n, p) = StdIn.readLine().split(" ").map(_.toInt)
+        try {
+          val ans = myCalculator.power(n, p)
+          Console.println(ans);
+        }
+        catch {
+          case e: Exception => Console.println(e.getMessage)
+        }
+        T -= 1
+      }
+    }
+  }
+
+  test("Run solution") {
+
+    val inputData = mutable.Queue(
+      "4",
+      "3 5",
+      "2 4",
+      "-1 -2",
+      "-1 3"
+    )
+    StdIn.setInputData(inputData)
+
+    val outputData = mutable.Queue(
+      "243",
+      "16",
+      "n and p should be non-negative",
+      "n and p should be non-negative"
+    )
+
+    Console.setOutputData(outputData)
+
+    Solution.main(null)
+  }
+}
---
+++ src/src/test/scala/hackerrank/thirtydaysofcode/Day18_Queues_and_Stacks.scala
@@ -0,0 +1,78 @@
+package hackerrank.thirtydaysofcode.scala
+
+import hackerrank.helper.scala._
+import org.scalatest.FunSuite
+
+import scala.collection.mutable
+
+class Day18_Queues_and_Stacks extends FunSuite {
+
+  object Solution {
+
+    class Solution {
+
+      var _s = mutable.Stack[Char]()
+      var _q = mutable.Queue[Char]()
+
+      def pushCharacter(c: Char) = _s.push(c)
+      def enqueueCharacter(c: Char) = _q.enqueue(c)
+      def dequeueCharacter(): Char = _q.dequeue()
+      def popCharacter(): Char = _s.pop()
+    }
+
+    import scala.util.control._
+
+    def main(args: Array[String]) {
+      // read the string s
+      val s = StdIn.readLine()
+      // create the Solution class object p
+      val obj = new Solution()
+      var i = 0
+      val len = s.length()
+      //push/enqueue all the characters of string s to stack
+      for (i <- 0 until len) {
+        obj.pushCharacter(s.charAt(i))
+        obj.enqueueCharacter(s.charAt(i))
+      }
+
+      var isPalindrome = true
+
+      /*pop the top character from stack
+        dequeue the first character from queue
+        compare both the characters*/
+      val loop = new Breaks
+      loop.breakable {
+        for (i <- 0 to len / 2) {
+          if (obj.popCharacter() != obj.dequeueCharacter()) {
+            isPalindrome = false
+            loop.break
+          }
+        }
+      }
+
+      //finally print whether string s is palindrome or not
+      if (isPalindrome) {
+        Console.println("The word, " + s + ", is a palindrome.")
+      }
+      else {
+        Console.println("The word, " + s + ", is not a palindrome.")
+      }
+    }
+  }
+
+  test("Run solution") {
+
+    val inputData = mutable.Queue(
+      "racecar"
+    )
+    StdIn.setInputData(inputData)
+
+    val outputData = mutable.Queue(
+      "The word, racecar, is a palindrome."
+    )
+
+    Console.setOutputData(outputData)
+
+    Solution.main(null)
+  }
+}
